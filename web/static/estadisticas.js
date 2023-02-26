ecd63b8edd7547a8ba7e366265dc2425e157165c
dma1004 <dma1004@alu.ubu.es>
2023-02-26 19:46:16 +0100

    Gráfico unificado de estadísticas
    Ahora solo hay un único gráfico de las estadísticas, el usuario puede seleccionar mediante unos checkboxes cuál quiere ver.

--- web/static/estadisticas.js
+++ web/static/estadisticas.js
@@ -1,14 +1,12 @@
 
-function estadisticagenerica(datos,id,y) {
+function estadisticagenerica(stats) {
 
-    let lista = crearListaStat(JSON.parse(datos.stats),id);
-
-    let margin = {top: 10, right: 30, bottom: 40, left: 60},
-        width = 850 - margin.left - margin.right,
+    let margin = {top: 10, right: 120, bottom: 40, left: 50},
+        width = 950 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
 
-    let statsvg = d3.select("#"+id)
+    let statsvg = d3.select("#graficoestadisticas")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -47,18 +45,56 @@ function estadisticagenerica(datos,id,y) {
         .attr("x", -height / 2)
         .attr("dy", "1em")
         .attr("transform", "rotate(-90)")
-        .text(y);
+        .text("Valor");
+
+    let color = d3.scaleOrdinal()
+        .domain(stats)
+        .range(d3.schemeCategory10);
+
+    statsvg.append('g')
+        .attr("id","leyenda")
+        .selectAll("target")
+        .data(stats)
+        .enter()
+        .append("text")
+        .attr("x", 120)
+        .attr("y", function(d,i){ return 100 + i*25;})
+        .style("fill", function(d){ return color(d);})
+        .text(function(d){ return d;})
+        .style("alignment-baseline", "top")
+        .attr("transform", "translate(" + (width -110) + "," + -90 + ")");
+
+    return color
+}
+
+function anadirestadistica(datos, stat, color) {
+    let lista = crearListaStat(JSON.parse(datos.stats),stat);
+
+    let margin = {top: 10, right: 30, bottom: 40, left: 60},
+        width = 850 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    let statsvg = d3.select("#graficoestadisticas").select("svg").select("g");
+
+    let statx = d3.scaleLinear()
+        .domain([0, maxit])
+        .range([0, width]);
+
+    let staty = d3.scaleLinear()
+        .domain([0, 1])
+        .range([height, 0]);
 
     let pts = statsvg.selectAll("dot")
         .data(lista)
         .enter()
         .append("circle")
+        .attr("id", stat)
         .attr("cx", function (d) { return statx(d[0]); } )
         .attr("cy", function (d) { return staty(d[1]); } )
         .attr("r", 5)
-        .attr("fill", "#69b3a2")
+        .attr("fill", color(stat))
         .style("visibility", function (d) {
-            if (d[0] <= cont){
+            if (d[0] <= cont && comprobarvisibilidad(stat)){
                 return "visible";
             }else{
                 return "hidden";
@@ -68,42 +104,48 @@ function estadisticagenerica(datos,id,y) {
     for (let i = 0; i < lista.length - 1; i++) {
         statsvg.append("line")
             .data([i+1])
-            .attr("id", "nueva")
+            .attr("id", stat)
             .attr("x1", statx(i))
             .attr("y1", staty(lista[i][1]))
             .attr("x2", statx(i + 1))
             .attr("y2", staty(lista[i + 1][1]))
-            .attr("stroke", "#69b3a2")
+            .attr("stroke", color(stat))
             .attr("stroke-width", 1.5)
-            .style("display", "none");
+            .style("display", function (d) {
+                if (d < cont && comprobarvisibilidad(stat)){
+                    return "block";
+                }else{
+                    return "none";
+                }
+            })
     }
 
-    let lineas = statsvg.selectAll('line[id="nueva"]');
+    let lineas = statsvg.selectAll('line[id='+stat+']');
 
     document.addEventListener('next', next);
     document.addEventListener('prev', prev);
 
 
     function next() {
-        pts.filter(function(d) {
-            return d[0] === cont;
-        })
-            .style("opacity", 0)
-            .transition()
-            .duration(600)
-            .style("opacity", 1)
-            .style("visibility", "visible")
+        if(comprobarvisibilidad(stat)) {
+            pts.filter(function (d) {
+                return d[0] === cont;
+            })
+                .style("opacity", 0)
+                .transition()
+                .duration(600)
+                .style("opacity", 1)
+                .style("visibility", "visible")
 
-        lineas.filter(function (d) {
-            return d === cont;
-        })
-            .attr("stroke-width", 0)
-            .transition()
-            .duration(600)
-            .attr("stroke-width", 1.5)
-            .style("display", "block");
-
-
+            lineas.filter(function (d) {
+                return d === cont;
+            })
+                .attr("stroke-width", 0)
+                .transition()
+                .duration(600)
+                .attr("stroke-width", 1.5)
+                .style("display", "block");
+        }
     }
 
     function prev() {
@@ -117,17 +159,42 @@ function estadisticagenerica(datos,id,y) {
         })
             .style("display", "none");
     }
-
 }
 
 
 function crearListaStat(stats,stat){
     let lista = [];
-    let aux = stats[stat];
+    let aux = stats[stat.toLowerCase()];
 
     for (let i = 0; i <= maxit; i++) {
         lista.push([i,aux[i]]);
     }
 
     return lista;
+}
+
+function habilitar(checked,stat) {
+    let statsvg = d3.select("#graficoestadisticas").select("svg").select("g");
+    let pts = statsvg.selectAll('circle[id='+stat+']');
+    let lineas = statsvg.selectAll('line[id='+stat+']');
+    if(checked){
+        pts.filter(function(d) {
+            return d[0] <= cont;
+        })
+            .style("visibility", "visible")
+
+        lineas.filter(function (d) {
+            return d <= cont;
+        })
+            .style("display", "block");
+    }else{
+        pts.style("visibility", "hidden")
+        lineas.style("display", "none");
+    }
+}
+
+function comprobarvisibilidad(stat) {
+    let check = document.getElementById("chk_" + stat);
+    console.log(check.checked)
+    return check.checked;
 }
