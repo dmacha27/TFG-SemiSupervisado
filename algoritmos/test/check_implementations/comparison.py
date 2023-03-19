fb95cbd6b0cc0491f4403b50e03e3d18fcd7009a
dma1004 <dma1004@alu.ubu.es>
2023-03-19 21:56:28 +0100

    Aplicar KFold y resultados en CSV
    Se ha sustituido la generación de los folds de forma manual por la de sklearn.
    Ahora se guardan los resultados en CSV, de momento solo Accuracy.

--- algoritmos/test/check_implementations/comparison.py
+++ algoritmos/test/check_implementations/comparison.py
@@ -1,33 +1,39 @@
+import copy
 from copy import deepcopy
-from math import ceil
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import sslearn.wrapper
 from numpy import mean, std
+from sklearn.model_selection import KFold
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_breast_cancer, load_diabetes, load_iris, load_digits, load_wine
 
 from algoritmos import CoTraining, DemocraticCoLearning, SelfTraining
-from algoritmos.utilidades.datasetloader import DatasetLoader
 from algoritmos.utilidades.datasplitter import data_split
 
 
-def cross_validation(clf1, clf2, x, y, folds, own_features, ssl_features=None):
+def cross_validation(own_clf, clf1_params, other_clf, clf2_params, x, y, folds, own_features, comparison_name, ssl_features=None):
     """
     Realiza el proceso de validación cruzada manualmente para comparar modelo de implementación propia contra uno
     de sslearn
 
-    :param clf1: Primer modelo de entrenamiento (implementación propia)
-    :param clf2: Segundo modelo de entrenamiento (sslearn)
+    :param own_clf: Primer modelo de entrenamiento (implementación propia).
+    :param clf1_params: Parámetros para el primer modelo.
+    :param other_clf: Segundo modelo de entrenamiento (sslearn).
+    :param clf2_params: Parámetros para el segundo modelo.
     :param x: Instancias.
     :param y: Etiquetas de las instancias.
-    :param own_features: Etiquetas de las características (implementación propia)
+    :param folds: Número de folds.
+    :param own_features: Etiquetas de las características (implementación propia).
+    :param comparison_name: Nombre de la comparación.
     :param ssl_features: Índices de las características (sslearn)
     :return: Exactitud de ambos modelos (mismo orden que de entrada) junto con ambas desviaciones estándar
     """
+
     (
         x,
         y,
@@ -35,61 +41,52 @@ def cross_validation(clf1, clf2, x, y, folds, own_features, ssl_features=None):
         y_test
     ) = data_split(x, y, is_unlabelled=False, p_unlabelled=0.8, p_test=0.2)
 
+    #exit()
     first_i_u = np.where(y == -1)[0][0]
     x_labelled = x[:first_i_u]
     x_unlabelled = x[first_i_u:]
 
     y = y[:first_i_u]
 
+    # En x e y solo hay datos etiquetados
     x = np.concatenate((x_labelled, x_test), axis=0)
     y = np.concatenate((y, y_test))
 
-    fold_step = ceil(len(x) / folds)
+    kf = KFold(n_splits=10)
 
-    folds_x = []
-    folds_y = []
-    clf1_clones = []
-    clf2_clones = []
-    for i in range(folds):
-        folds_x.append(x[i * fold_step: min(len(y), (i + 1) * fold_step) + 1])
-        folds_y.append(y[i * fold_step: min(len(y), (i + 1) * fold_step) + 1])
-        clf1_clones.append(deepcopy(clf1))
-        clf2_clones.append(deepcopy(clf2))
-
+    stats_clf1 = pd.DataFrame(columns=['accuracy'])
+    stats_clf2 = pd.DataFrame(columns=['accuracy'])
     accuracy_clf1 = []
     accuracy_clf2 = []
-    for i in range(folds):
-        x_test = folds_x[i]
-        y_test = folds_y[i]
+    for train_index, test_index in kf.split(x):
+        x_test = x[test_index]
+        y_test = y[test_index]
 
-        left_folds_x = np.concatenate(folds_x[0:i], axis=0) if i != 0 else [[]]
-        left_folds_y = np.concatenate(folds_y[0:i]) if i != 0 else []
+        x_train = x[train_index]
+        y_train = y[train_index]
+        x_train = np.concatenate((x_train, x_unlabelled), axis=0)
+        y_train = np.concatenate((y_train, [-1] * len(x_unlabelled)))
 
-        right_folds_x = np.concatenate(folds_x[i + 1:len(folds_x)], axis=0) if i != folds - 1 else [[]]
-        right_folds_y = np.concatenate(folds_y[i + 1:len(folds_y)]) if i != folds - 1 else []
+        clf1 = own_clf(**copy.deepcopy(clf1_params))
 
-        if i == 0:
-            x = right_folds_x
-            y = right_folds_y
-        elif i == folds - 1:
-            x = left_folds_x
-            y = left_folds_y
-        else:
-            x = np.concatenate((left_folds_x, right_folds_x), axis=0)
-            y = np.concatenate((left_folds_y, right_folds_y))
+        clf1.fit(x_train, y_train, x_test, y_test, own_features)  # Implementación
 
-        x = np.concatenate((x, x_unlabelled), axis=0)
-        y = np.concatenate((y, [-1] * len(x_unlabelled)))
-
-        clf1_clones[i].fit(x, y, x_test, y_test, own_features)  # Implementación
+        clf2 = other_clf(**copy.deepcopy(clf2_params))
         if ssl_features:
-            clf2_clones[i].fit(x, y, features=ssl_features)  # sslearn
+            clf2.fit(x_train, y_train, features=ssl_features)  # sslearn
         else:
-            clf2_clones[i].fit(x, y)
+            clf2.fit(x_train, y_train)
 
-        accuracy_clf1.append(clf1_clones[i].get_accuracy_score(x_test, y_test))
-        accuracy_clf2.append(clf2_clones[i].score(x_test, y_test))
+        acc = clf1.get_accuracy_score(x_test, y_test)
+        accuracy_clf1.append(acc)
+        stats_clf1.loc[len(stats_clf1)] = acc
 
+        acc = clf2.score(x_test, y_test)
+        accuracy_clf2.append(acc)
+        stats_clf2.loc[len(stats_clf2)] = acc
+
+    stats_clf1.to_csv(path_or_buf=f'./results/{comparison_name}-own', index=False)
+    stats_clf2.to_csv(path_or_buf=f'./results/{comparison_name}-sslearn', index=False)
     return mean(accuracy_clf1), std(accuracy_clf1), mean(accuracy_clf2), std(accuracy_clf2)
 
 
@@ -100,16 +97,15 @@ def selftraining_comparison(data):
 
     :return: Accuracy de ambos modelos después del entrenamiento
     """
-
-    df = pd.DataFrame(data.data, columns=data.feature_names)
-    df['target'] = pd.Series(data.target)
-
-    return cross_validation(SelfTraining(GaussianNB(), th=0.75, n_iter=10),
-                            sslearn.wrapper.SelfTraining(GaussianNB(), threshold=0.75, max_iter=10),
-                            df[data.feature_names],
-                            df['target'],
+    return cross_validation(SelfTraining,
+                            {'clf': GaussianNB(), 'th': 0.75, 'n_iter': 10},
+                            sslearn.wrapper.SelfTraining,
+                            {'base_estimator': GaussianNB(), 'threshold': 0.75, 'max_iter': 10},
+                            data.data,
+                            data.target,
                             10,
-                            data.feature_names)
+                            data.feature_names,
+                            "SelfTraining")
 
 
 def cotraining_comparison(data):
@@ -119,18 +115,21 @@ def cotraining_comparison(data):
 
     :return: Accuracy de ambos modelos después del entrenamiento
     """
+    n_features = len(data.feature_names)
 
-    df = pd.DataFrame(data.data, columns=data.feature_names)
-    df['target'] = pd.Series(data.target)
-
-    return cross_validation(CoTraining(clf1=DecisionTreeClassifier(),
-                                       clf2=GaussianNB(), p=1, n=3, u=75, n_iter=30),
-                            sslearn.wrapper.CoTraining(second_base_estimator=GaussianNB()),
-                            df[data.feature_names],
-                            df['target'],
+    return cross_validation(CoTraining,
+                            {'clf1': DecisionTreeClassifier(), 'clf2': DecisionTreeClassifier(),
+                             'p': 1, 'n': 3, 'u': 75, 'n_iter': 30},
+                            sslearn.wrapper.CoTraining,
+                            {'second_base_estimator': DecisionTreeClassifier()},
+                            data.data,
+                            data.target,
                             10,
                             data.feature_names,
-                            [[0, 1, 2, 3, 4], [5, 6, 7, 8]])
+                            "CoTraining",
+                            [list(range(n_features // 2 + 1)),
+                             list(range(n_features // 2 + 1,
+                                        n_features))])
 
 
 def democraticolearning_comparison(data):
@@ -141,9 +140,6 @@ def democraticolearning_comparison(data):
     :return: Accuracy de ambos modelos después del entrenamiento
     """
 
-    df = pd.DataFrame(data.data, columns=data.feature_names)
-    df['target'] = pd.Series(data.target)
-
     clfs = [SVC(kernel='rbf',
                 probability=True,
                 C=1.0,
@@ -151,21 +147,35 @@ def democraticolearning_comparison(data):
                 random_state=0
                 ), GaussianNB(), DecisionTreeClassifier()]
 
-    dcl = DemocraticCoLearning(clfs=deepcopy(clfs))
-
-    dclssl = sslearn.wrapper.DemocraticCoLearning(base_estimator=deepcopy(clfs))
-
-    return cross_validation(dcl,
-                            dclssl,
-                            df[data.feature_names],
-                            df['target'],
+    return cross_validation(DemocraticCoLearning,
+                            {'clfs': deepcopy(clfs)},
+                            sslearn.wrapper.DemocraticCoLearning,
+                            {'base_estimator': deepcopy(clfs)},
+                            data.data,
+                            data.target,
                             10,
-                            data.feature_names, )
+                            data.feature_names,
+                            "DemocraticCoLearning")
+
+
+def draw_comparison(comparison_name):
+    stats_clf1 = pd.read_csv(f'./results/{comparison_name}-own')
+    stats_clf2 = pd.read_csv(f'./results/{comparison_name}-sslearn')
+
+    plt.plot(stats_clf1.index, stats_clf1['accuracy'], label="Implementación")
+    plt.plot(stats_clf2.index, stats_clf2['accuracy'], label="sslearn")
+    plt.xlabel('Fold')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.show()
 
 
 if __name__ == '__main__':
     data = load_breast_cancer()
 
+    draw_comparison("DemocraticCoLearning")
+    exit()
     print("---Self-Training---")
     own, std_own, ssl, std_ssl = selftraining_comparison(data)
 
@@ -179,7 +189,6 @@ if __name__ == '__main__':
     print(f"Implementación sslearn: {ssl} ({std_ssl})")
 
     print("---Democratic Co-Learning---")
-
     own, std_own, ssl, std_ssl = democraticolearning_comparison(data)
 
     print(f"Implementación propia: {own} ({std_own})")
