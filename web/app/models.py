477e4ba01c290bcee9c1fe0df92cf70052daf913
dma1004 <dma1004@alu.ubu.es>
2023-05-14 16:01:33 +0200

    Añadir base de datos al anexo diseño

--- web/app/models.py
+++ web/app/models.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(40), unique=True)
     name = db.Column(db.String(10))
-    password = db.Column(db.String(20))
+    password = db.Column(db.String(64))
     admin = db.Column(db.Boolean, default=False)
     last_login = db.Column(db.DateTime, default=func.now())
     datasets = relationship("Dataset", back_populates='user')
