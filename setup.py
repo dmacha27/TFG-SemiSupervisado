b8b573df18e220fc34f1f5d5e4dedf66fbfeeaaf
dma1004 <dma1004@alu.ubu.es>
2023-06-10 21:00:00 +0200

    Actualizar versión algoritmos y anexos
    Versión 1.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='algoritmos',
-    version='0.1',
+    version='1.0',
     packages=find_packages(),
     author='David Martínez Acha',
     description='Paquete de algoritmos Semi-Supervisados'
